12ed778ba16ac9fff00a22878200b76f9ccab2a9
Pratik2254 <95663655+Pratik2254@users.noreply.github.com>
2023-06-16 08:31:33 +0545

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.Diagnostics;
+using TSAIdentity.Data;
+using TSAIdentity.Models;
+
+namespace TSAIdentity.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ILogger<HomeController> logger, UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager ,ApplicationDbContext context)
+        {
+            _logger = logger;
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                // Redirect to the Dashboard action of your desired controller
+                return RedirectToAction("Dashboard", "Home");
+            }
+            return View();
+        }
+        public async Task<IActionResult> DashBoardAsync()
+        {
+            string userEmail = HttpContext.User.Identity.Name;
+
+            if (userEmail == null || _context.Organizations == null)
+            {
+                return NotFound();
+            }
+            if (User.IsInRole("Admin"))
+            {
+                var organization = await _context.Organizations
+                .FirstOrDefaultAsync(o => o.OrganizationEmail == userEmail);
+                if (organization == null)
+                {
+                    return NotFound();
+                }
+                var OrganizationId = organization.OrganizationId;
+
+                ViewData["Designation_Count"] = _context.Designations.Count(d => d.OrganizationId == OrganizationId);
+                ViewData["Skill_Count"] = _context.Skills.Count(s => s.OrganizationId == OrganizationId);
+                ViewData["Active_Employees"] = _context.Employees.Count(e => e.OrganizationId == OrganizationId && e.IsActive);
+                ViewData["Busy_Employees"] = _context.Employees.Count(e => e.OrganizationId == OrganizationId && e.IsActive && e.IsBusy);
+                ViewData["Available_Employees"] = _context.Employees.Count(e => e.OrganizationId == OrganizationId && e.IsActive && !e.IsBusy);
+                ViewData["Total_Project"] = _context.Projects.Count(p => p.OrganizationId == OrganizationId);
+                ViewData["Pending_Project"] = _context.Projects.Count(p => p.OrganizationId == OrganizationId && p.ProjectStatus == Models.ProjectStatus.Pending);
+                ViewData["Ongoing_Project"] = _context.Projects.Count(p => p.OrganizationId == OrganizationId && p.ProjectStatus == Models.ProjectStatus.InProgress);
+                ViewData["Completed_Project"] = _context.Projects.Count(p => p.OrganizationId == OrganizationId && p.ProjectStatus == Models.ProjectStatus.Completed);
+            }   
+            else if (User.IsInRole("Employee"))
+            {
+                var employee = await _context.Employees
+                .FirstOrDefaultAsync(e => e.EmployeeEmail == userEmail);
+                if (employee == null)
+                {
+                    return NotFound();
+                }
+                ViewData["TotalAssigned"]= _context.Tasks.Count(t=>t.AssignedEmployeeId== employee.EmployeeId);
+                ViewData["Completed"] = _context.Tasks.Count(t => t.AssignedEmployeeId == employee.EmployeeId && t.TaskStatus==Models.TaskStatus.Completed);
+                ViewData["InProgress"] = _context.Tasks.Count(t => t.AssignedEmployeeId == employee.EmployeeId && (t.TaskStatus == Models.TaskStatus.Assigned|| t.TaskStatus==Models.TaskStatus.InProgress ) );
+                ViewData["Assigned"] = await _context.Tasks.AnyAsync(t => t.AssignedEmployeeId == employee.EmployeeId && t.TaskStatus == Models.TaskStatus.Assigned);
+            }
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        
+    }
+}
---
+++ Controllers/TasksController.cs
@@ -0,0 +1,374 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Build.Evaluation;
+using Microsoft.Build.Framework;
+using Microsoft.CodeAnalysis;
+using Microsoft.EntityFrameworkCore;
+using TSAIdentity.Data;
+using TSAIdentity.Models;
+
+namespace TSAIdentity.Controllers
+{
+    public class TasksController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TasksController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Tasks
+        //public async Task<IActionResult> Index()
+        //{
+        //    var applicationDbContext = _context.Tasks.Include(t => t.AssignedEmployee).Include(t => t.Organization).Include(t => t.Project).Include(t => t.Skill);
+        //    return View(await applicationDbContext.ToListAsync());
+        //}
+
+        // GET: Tasks/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null || _context.Tasks == null)
+            {
+                return NotFound();
+            }
+
+            var tasks = await _context.Tasks
+                .Include(t => t.AssignedEmployee)
+                .Include(t => t.Organization)
+                .Include(t => t.Project)
+                .Include(t => t.Skill)
+                .FirstOrDefaultAsync(m => m.TaskId == id);
+            if (tasks == null)
+            {
+                return NotFound();
+            }
+
+            return View(tasks);
+        }
+
+        // GET: Tasks/Create
+        public async Task<IActionResult> CreateAsync(Guid ?id)
+        {
+            var project = await _context.Projects.FindAsync(id);    
+            ViewData["OrganizationId"] = project.OrganizationId;
+            ViewData["ProjectId"] = id;
+            TempData["projectId"] = id;
+            ViewData["SkillId"] = new SelectList(_context.Skills.Where(e=>e.OrganizationId==project.OrganizationId), "SkillId", "SkillName");
+            return View();
+        }
+
+        // POST: Tasks/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("TaskId,TaskName,TaskDescription,SkillId,ProjectId,OrganizationId")] Tasks tasks)
+        {
+            var userEmail = HttpContext.User.Identity?.Name;
+            var organization = await _context.Organizations
+                          .Where(o => o.OrganizationEmail == userEmail)
+                          .FirstOrDefaultAsync();
+            bool taskExists = await _context.Tasks.AnyAsync(t => t.TaskName == tasks.TaskName && t.OrganizationId == organization.OrganizationId && t.ProjectId==tasks.ProjectId);
+
+            if (taskExists)
+            {
+                ModelState.AddModelError("TaskName", "A task with the same name already exists.");
+                return View(tasks);
+            }
+            if (ModelState.IsValid)
+            {
+                tasks.TaskId = Guid.NewGuid();
+                _context.Add(tasks);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Details","Projects", new { id = tasks.ProjectId });
+            }
+            ViewData["SkillId"] = new SelectList(_context.Skills.Where(e => e.OrganizationId == tasks.OrganizationId), "SkillId", "SkillName", tasks.SkillId);
+            return View(tasks);
+        }
+
+        // GET: Tasks/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null || _context.Tasks == null)
+            {
+                return NotFound();
+            }
+
+            var tasks = await _context.Tasks.FindAsync(id);
+            if (tasks == null)
+            {
+                return NotFound();
+            }
+            
+            ViewData["SkillId"] = new SelectList(_context.Skills.Where(e => e.OrganizationId == tasks.OrganizationId), "SkillId", "SkillName", tasks.SkillId);
+            return View(tasks);
+        }
+
+        // POST: Tasks/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("TaskId,TaskName,TaskDescription,TaskStatus,isassigned,SkillId,AssignedEmployeeId,ProjectId,OrganizationId")] Tasks tasks)
+        {
+            if (id != tasks.TaskId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                var userEmail = HttpContext.User.Identity?.Name;
+                var organization = await _context.Organizations
+                              .Where(o => o.OrganizationEmail == userEmail)
+                              .FirstOrDefaultAsync();
+                bool taskExists = await _context.Tasks.AnyAsync(t => t.TaskName == tasks.TaskName && t.TaskId != id && t.OrganizationId==organization.OrganizationId && t.ProjectId == tasks.ProjectId);
+
+                if (taskExists)
+                {
+                    ModelState.AddModelError("TaskName", "A task with the same name already exists.");
+                    return View(tasks);
+                }
+                try
+                {
+                    _context.Update(tasks);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TasksExists(tasks.TaskId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Details", "Projects", new { id = tasks.ProjectId });
+            }
+            ViewData["SkillId"] = new SelectList(_context.Skills.Where(s => s.OrganizationId == tasks.OrganizationId), "SkillId", "SkillName", tasks.SkillId);
+            return View(tasks);
+        }
+
+        // GET: Tasks/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null || _context.Tasks == null)
+            {
+                return NotFound();
+            }
+
+            var tasks = await _context.Tasks
+                .Include(t => t.AssignedEmployee)
+                .Include(t => t.Organization)
+                .Include(t => t.Project)
+                .Include(t => t.Skill)
+                .FirstOrDefaultAsync(m => m.TaskId == id);
+            if (tasks == null)
+            {
+                return NotFound();
+            }
+
+            return View(tasks);
+        }
+
+        // POST: Tasks/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            if (_context.Tasks == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Tasks'  is null.");
+            }
+            var tasks = await _context.Tasks.FindAsync(id);
+            if (tasks != null)
+            {
+                _context.Tasks.Remove(tasks);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Details", "Projects", new { id = tasks.ProjectId });
+        }
+
+        private bool TasksExists(Guid id)
+        {
+          return (_context.Tasks?.Any(e => e.TaskId == id)).GetValueOrDefault();
+        }
+        public async Task<IActionResult> AssignTask(Guid id)
+        {
+            var task = await _context.Tasks.FindAsync(id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            var viewModel = new AssignTaskViewModel
+            {
+                TaskId = task.TaskId,
+                TaskName = task.TaskName,
+            };
+            var userEmail = HttpContext.User.Identity?.Name;
+            var organization = await _context.Organizations
+                          .Where(o => o.OrganizationEmail == userEmail)
+                          .FirstOrDefaultAsync();
+            ViewData["EmployeeId"] = new SelectList(_context.Employees.Where(e => e.OrganizationId == organization.OrganizationId && !e.IsBusy && _context.EmployeeSkills.Any(es => es.EmployeeId == e.EmployeeId && es.SkillId == task.SkillId)), "EmployeeId", "EmployeeName");
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AssignTask(AssignTaskViewModel viewModel)
+        {
+            //return Content(viewModel.TaskId.ToString()+","+viewModel.TaskName+","+viewModel.SelectedEmployeeId.ToString());
+            if (ModelState.IsValid)
+            {
+                var task = await _context.Tasks.FindAsync(viewModel.TaskId);
+                if (task == null)
+                {
+                    return NotFound();
+                }
+
+                task.AssignedEmployeeId = viewModel.SelectedEmployeeId;
+                task.isassigned = true;
+                task.TaskStatus= Models.TaskStatus.Assigned;
+                _context.Update(task);
+                
+
+                var employee = await _context.Employees.FindAsync(viewModel.SelectedEmployeeId);
+                employee.IsBusy = true;
+                _context.Update(employee);
+
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction("Details","Projects", new { id = task.ProjectId });
+            }
+
+            // If ModelState is invalid, redisplay the form with validation errors
+            var userEmail = HttpContext.User.Identity?.Name;
+            var organization = await _context.Organizations
+                          .Where(o => o.OrganizationEmail == userEmail)
+                          .FirstOrDefaultAsync();
+            ViewData["EmployeeId"] = new SelectList(_context.Employees.Where(e => e.OrganizationId == organization.OrganizationId), "EmployeeId", "EmployeeName",viewModel.SelectedEmployeeId);
+
+            return View(viewModel);
+        }
+
+
+        public async Task<IActionResult> DeassignTask(Guid id)
+        {
+            var task = await _context.Tasks.FindAsync(id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+            var employee = await _context.Employees.FindAsync(task.AssignedEmployeeId);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            task.AssignedEmployeeId = null;
+            task.isassigned = false;
+            task.TaskStatus = Models.TaskStatus.Pending;
+            _context.Update(task);
+
+            employee.IsBusy = false;
+            _context.Update(employee);
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Details", "Projects", new { id = task.ProjectId });
+        }
+
+        [Authorize(Roles = "Employee")]
+        public async Task<IActionResult> AssignedTasksAsync()
+        {
+            // Get the current employee's ID
+            string userEmail = HttpContext.User.Identity.Name;
+
+            if (userEmail == null)
+            {
+                return NotFound();
+            }
+            var employee = await _context.Employees.FirstOrDefaultAsync(e => e.EmployeeEmail == userEmail);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            // Retrieve the tasks assigned to the employee from the database
+            var tasks = _context.Tasks
+                .Include(t => t.Project)
+                .Include(t => t.Skill)
+                .Where(t => t.AssignedEmployeeId == employee.EmployeeId)
+                .ToList();
+
+            return View(tasks);
+        }
+
+        public async Task<IActionResult> ChangeStatusAsync(Guid id)
+        {
+            var task = _context.Tasks.FirstOrDefault(t => t.TaskId == id);
+
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            if (task.TaskStatus == Models.TaskStatus.Assigned)
+            {
+                task.TaskStatus = Models.TaskStatus.InProgress;
+                _context.Update(task);
+
+                // Retrieve the associated project
+                var project = _context.Projects.FirstOrDefault(p => p.ProjectId == task.ProjectId);
+                if (project != null)
+                {
+                    project.ProjectStatus = ProjectStatus.InProgress;
+                    _context.Update(project);
+                }
+            }
+            else if (task.TaskStatus == Models.TaskStatus.InProgress)
+            {
+                task.TaskStatus = Models.TaskStatus.Completed;
+
+                var employee = await _context.Employees.FindAsync(task.AssignedEmployeeId);
+                if (employee != null)
+                {
+                    employee.IsBusy = false;
+                    _context.Update(employee);
+                }
+
+                var project = await _context.Projects.FindAsync(task.ProjectId);
+                if (project != null)
+                {
+                    bool allTasksCompleted = await _context.Tasks.Where(t => t.ProjectId == project.ProjectId &&t.TaskId != task.TaskId).AllAsync(t => t.TaskStatus == Models.TaskStatus.Completed);
+
+                    if (allTasksCompleted)
+                    {
+                        // Update project status to completed
+
+                        if (project != null)
+                        {
+                            project.ProjectStatus = ProjectStatus.Completed;
+                            _context.Update(project);
+                            await _context.SaveChangesAsync();
+                        }
+                    }
+                }
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToAction("AssignedTasks");
+        }
+
+    }
+}
